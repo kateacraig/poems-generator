40608b865ab78a88c6642f1929b7415eb9ebe3d8
kateacraig <kate.a.craig@gmail.com>
2025-07-02 17:05:44 -0400

    Added basic HTML classes and JS for the form with typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function poemGenerator(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "I had no thought of violets of late",
+    autoStart: true,
+    delay: 35,
+    cursor: null,
+  });
+}
 
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", poemGenerator);
