e265f7693b82df9fb6c33853b9b85dd696d48b9c
kateacraig <kate.a.craig@gmail.com>
2025-07-06 09:07:17 -0400

    Updated the AI context to translate the poem to Spanish and removed the HTML code where it was first going to go.
    Also removed the JS code for the initial functions for the Spanish translation.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,4 @@
-function displayNewPoem{
-    console.log("poem generated");
-
-  new Typewriter("#spanish-poem", {
-    strings: translatePoem(response.data.answer),
-    autoStart: true,
-    delay: 50,
-    cursor: null,
-    loop: false,
-  }); 
-}
-
-function translatePoem (response){
-    let newApiKey = "4a31b94f5oa0a28ee30tf2fd8208d6b6";
-    let newPrompt = displayPoem(response.data.answer);
-    let newContext = `Translate ${displayPoem(response.data.answer)} into Spanish.`;
-    let newApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${newPrompt}&context=${newContext}&key=${newApiKey}`;
-
-    console.log("Generating poem");
-    console.log(`Prompt: ${newPrompt}`);
-    console.log(`Context: ${newContext}`);
-
-     axios(newApiUrl).then(displayNewPoem);
-}
-
 function displayPoem(response) {
-
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -32,8 +6,6 @@ function displayPoem(response) {
     cursor: null,
     loop: false,
   });
-
-  function translatePoem();
 }
 
 function poemGenerator(event) {
@@ -41,7 +13,7 @@ function poemGenerator(event) {
 
   let apiKey = "4a31b94f5oa0a28ee30tf2fd8208d6b6";
   let context =
-    "You are nature poet who loves to write short poems. Your mission is to generate a 4 line poem in HTML and separate each line with a <br />. Make sure to follow the user input. Do not include a poem title. Sign the poem with 'SheCodes AI' at the end of the poem (NOT at the beginning) after two <br/>s inside a <strong> element. Do NOT write '```HTML' at the beginning of the poem. Do not write '```' at the end of the poem or the end of signing the poem.";
+    "You are nature poet who loves to write short poems. Your mission is to generate a 4 line English poem in HTML and separate each line with a <br />. Make sure to follow the user input. Do not include a poem title. Then add 1 <br/> and then translate the English poem into Spanish, using basic HTML and separating each line using a <br/> following the same 4 line format. 1 line above the Spanish translated poem, add '(Translated to Spanish)' with a font style of italics and a font weight of 300. Sign the poem with 'SheCodes AI' at the end of the Spanish poem (NOT at the beginning) after 2 <br/>s inside a <strong> element. Do NOT write '```HTML' at the beginning of the poem. Do not write '```' at the end of the poem or the end of signing the poem.";
   let poemInput = document.querySelector("#poem-input");
   let prompt = `User input is: Write a poem about ${poemInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
